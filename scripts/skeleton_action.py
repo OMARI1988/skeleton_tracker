6aa357248d6b8e9565660703a12ec12224056db2
PDuckworth <scpd@leeds.ac.uk>
2016-06-30 17:04:43 +0100

    updated to use serices for consent

--- scripts/skeleton_action.py
+++ scripts/skeleton_action.py
@@ -13,6 +13,8 @@ from skeleton_publisher import SkeletonManager
 from data_logging import SkeletonImageLogger
 from skeleton_tracker.msg import skeletonAction, skeletonActionResult, skeleton_message
 from skeleton_tracker.srv import *
+from std_srvs.srv import Empty, EmptyResponse
+from consent_tsc.msg import ManageConsentAction, ConsentResult, ManageConsentGoal
 
 class skeleton_server(object):
     def __init__(self):
@@ -38,107 +40,126 @@ class skeleton_server(object):
         self.ptu_action_client.wait_for_server()
 
         # publishers
-        self.publish_rec = rospy.Publisher('skeleton_data/recording_started', String, queue_size = 1)
-        self.publish_rec.publish("init")
+        # self.publish_rec = rospy.Publisher('skeleton_data/recording_started', String, queue_size = 1)
+        # self.publish_rec.publish("init")
 
-        #request_sent
-        self.request_sent_flag = 0
+
+    def consent_client(self):
+        rospy.loginfo("Creating consent client")
+        ret = None
+        try:
+            consent_client = actionlib.SimpleActionClient('manage_consent', ManageConsentAction)
+            if consent_client.wait_for_server(timeout=rospy.Duration(10)):
+                goal = ManageConsentGoal()
+                consent_client.send_goal(goal)
+    
+                # here you should check whether you've been preempted, shutdown etc. while waiting for consent
+                while True:
+                    if consent_client.wait_for_result(timeout = rospy.Duration(1)):
+                        result = consent_client.get_result()
+                        int_result = result.result.result
+    
+                        if int_result == ConsentResult.DEPTH_AND_RGB:
+                            # print 'depth+rgb'
+                            ret = "everything"
+                            
+                        elif int_result == ConsentResult.DEPTH_ONLY:
+                            # print 'just depth'
+                            ret = "depthskel"
+                        else:
+                            # print 'no consent'
+                            ret = "nothing"
+                        break
+            else:
+                rospy.logwarn('No manage consent server')
+        except Exception, e:
+            rospy.logwarn('Exception when trying to manage consent: %s' % e)
+        return ret
+  
+    def signal_start_of_recording(self):
+        rospy.wait_for_service('signal_recording_started', timeout=10)
+        signal_recording_started = rospy.ServiceProxy('signal_recording_started', Empty)
+        # tell the webserver to say we've started recording
+        signal_recording_started()
 
 
     def execute_cb(self, goal):
+    
+        self.signal_start_of_recording()
         duration = goal.duration
         start = rospy.Time.now()
         end = rospy.Time.now()
-        self.publish_rec.publish("started_rec_callback")   #the cb for this shows the recording webpage
         self.image_logger.stop_image_callbacks = 0   #start the image callbacks in the logger
         self.sk_publisher._initialise_data()
         self.sk_publisher.robot_pose_flag = 1
 
         print goal
-        #initialise things
         self.set_ptu_state(goal.waypoint)
         prev_uuid = ""
         self.skeleton_msg.uuid = ""
-        self.image_logger.request_sent_flag = 0
-        self.image_logger.consent_ret = None
-        self.image_logger.consent_subscriber = rospy.Subscriber("/skeleton_data/consent_ret", String, callback=self.image_logger.consent_ret_callback, queue_size=1)
 
-        #thread = None
+        consent_msg = None
+        request_consent = 0
+        
         while (end - start).secs < duration.secs:
             if self._as.is_preempt_requested():
-                #self.image_logger.stop = True
-                break
-                # return self._as.set_preempted()
+                 break
                 
-            #print self.image_logger.consent_ret
-            consented = self.image_logger.consent_ret
-            if self.image_logger.request_sent_flag != 1:
+            if consent_msg is None and request_consent is 0:
                 self.sk_publisher.publish_skeleton()
-                rospy.sleep(0.01)  # wait until something is published
+                # rospy.sleep(0.01)  # wait until something is published
 
                 #when a skeleton incremental msg is received
                 if self.skeleton_msg.uuid != "":
-
-                    #print "tracking person: ", self.skeleton_msg.uuid
                     prev_uuid = self.skeleton_msg.uuid
                     self.sk_publisher.logged_uuid = prev_uuid
-
-                    if self.image_logger.request_sent_flag != 1:
-                        self.image_logger.callback(self.skeleton_msg, goal.waypoint)
-                        #thread = threading.Thread(
-                        #    target=self.image_logger.callback,
-                        #    args=(self.skeleton_msg, goal.waypoint,)
-                        #)
-                        #thread.start()
-
-            else:
-                self.reset_ptu()
-                #print "here: ", self.image_logger.consent_ret 
-                #if self.image_logger.consent_ret != None:  #if consent is given:
-                if consented is not None:
-                    print "Consented. Break from action."
-                    #self.reset_ptu()
-                    break
+                    request_consent = self.image_logger.callback(self.skeleton_msg, goal.waypoint)
+            
+            elif consent_msg is not None:
+                print "breaking loop"
+                break
+                
+            elif request_consent is 1:
+            	self.reset_ptu()
+                print "Consent requested."
+                #call a simple actionlib server 
+                consent_msg = self.consent_client()
+                print "consent returned: %s" % consent_msg
+			    
+			   
             end = rospy.Time.now()
 
-
-        #if thread is not None:
-        #    thread.join()
-
-        # after the action reset everything
+        # after the action reset ptu and stop publisher
         self.reset_all()
-        self.image_logger.consent_subscriber.unregister()
 		
-        try:
-            self.image_logger.bag_file.close()
-        except AttributeError:
-            print "no bag file to close"
+        # try:
+        #     self.image_logger.bag_file.close()
+        # except AttributeError:
+        #     print "no bag file to close"
 
         if self._as.is_preempt_requested():
-            self.image_logger.stop = False
-            print "The action is being preempted, cancelling everything."
+            print "The action is being preempted, cancelling everything. \n"
             return self._as.set_preempted()
-        try:
-            previous_consent = self.image_logger.consent_ret.data
-        except AttributeError:  # if nothinging is returned :(
-            print "no consent given"
-            previous_consent = "nothing"
+        # try:
+        #     previous_consent = self.image_logger.consent_ret.data
+        # except AttributeError:  # if nothinging is returned :(
+        #     print "no consent given"
+        #     previous_consent = "nothing"
 
-        self.image_logger.consent_ret = None
         self._as.set_succeeded(skeletonActionResult())
 
+        print "call deleter with: %s " % consent_msg
+        
         try:
             proxy = rospy.ServiceProxy("/delete_images_service", DeleteImages)
             if prev_uuid != "":
-                req = DeleteImagesRequest(str(end), prev_uuid, str(previous_consent))
-                #print "deleting images..."
-
+                req = DeleteImagesRequest(str(end), prev_uuid, str(consent_msg))
                 proxy(req)
                 print "deleted..."
         except rospy.ServiceException:
             print "deleter service is not running. Moved if consent was given."
             if prev_uuid != "":
-                self.move_consented_data(prev_uuid, previous_consent)
+                self.move_consented_data(prev_uuid, consent_msg)
 
         print "finished action\n"
 
@@ -164,16 +185,14 @@ class skeleton_server(object):
 
 
     def reset_all(self):
-        self.image_logger.stop_image_callbacks = 0   #stop the image callbacks in the logger
+        # self.image_logger.stop_image_callbacks = 0   #stop the image callbacks in the logger
         self.sk_publisher.robot_pose_flag = 0        #stop the callbacks in the pub
         self.reset_ptu()
-        self.publish_rec.publish("finished")   #the cb for this shows the recording webpage
+        # self.publish_rec.publish("finished")   #the cb for this shows the recording webpage
         self.image_logger.go_back_to_where_I_came_from()
 
         ## remove data stored in the publisher (save memory)
         self.sk_publisher._initialise_data()
-        #self.sk_publisher.data = {}
-        #self.sk_publisher.users = {}
         self.sk_publisher.accumulate_data = {}
 
 
